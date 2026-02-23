cda1516640d052cb037467ea0800107fe19a6578
Johari57 <misnurulimanpdr@gmail.com>
2026-02-23 23:31:55 +0700

    Update yes button response text in scrips.js

--- scrips.js
+++ scrips.js
@@ -5,7 +5,7 @@ const yesBtn = document.getElementById("yes-btn");
 const noBtn = document.getElementById("no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "I knew it 😍";
+  question.innerHTML = " sudah gue tebak itu 😍";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
